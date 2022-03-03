64ecee4197492ca7c89481a303da7011833d792e
Rosendo <rosendo@tamu.edu>
2022-03-02 20:19:23 -0600

    not found error

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -73,4 +73,6 @@ Rails.application.configure do
 
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
+  ENV['GOOGLE_OAUTH_CLIENT_ID'] = '393976551863-rd86lpb5bpfvabsj1ffukuobr8dcsn60.apps.googleusercontent.com'
+  ENV['GOOGLE_OAUTH_CLIENT_SECRET'] = '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'
 end
