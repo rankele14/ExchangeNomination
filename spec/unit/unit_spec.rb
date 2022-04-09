c33032b6328e35255a89e135893e4bf001d36410
Raney <rankele14@gmail.com>
2022-04-09 14:30:53 -0500

    unit update
    update unit_spec.rb to account for testing email format. added validating email format to models with emails

--- spec/unit/unit_spec.rb
+++ spec/unit/unit_spec.rb
@@ -3,7 +3,7 @@ require 'spec_helper'
 
 RSpec.describe Representative, type: :model do
   subject do
-    @uni = University.new(university_name: 'AM')
+    @uni = University.new(university_name: 'AM', num_nominees: 0, max_limit: 3)
     @uni.save
     described_class.new(first_name: 'John', last_name: 'Smith', title: 'CEO', university_id: @uni.id, rep_email: 'JohnSmith@gmail.com')
   end
@@ -36,6 +36,59 @@ RSpec.describe Representative, type: :model do
     subject.rep_email = nil
     expect(subject).not_to be_valid
   end
+  
+  it 'does not allow emails that do not fit the specified format' do
+    invalid_email = 'user'
+    subject.rep_email = invalid_email
+    expect(subject).to be_invalid
+    
+    invalid_email = 'user@' 
+    subject.rep_email = invalid_email
+    expect(subject).not_to be_valid
+    
+    # this one fails
+    # invalid_email = 'user@foo' 
+    # subject.rep_email = invalid_email
+    # expect(subject).to be_invalid
+
+    # test it as valid (FIXME?)
+    unexpected_email = 'user@foo' 
+    subject.rep_email = unexpected_email
+    expect(subject).to be_valid
+    
+    invalid_email = 'user@foo,com'
+    subject.rep_email = invalid_email
+    expect(subject).not_to be_valid
+    
+    invalid_email = 'user_at_foo.org'
+    subject.rep_email = invalid_email
+    expect(subject).not_to be_valid
+    
+    invalid_email = 'user_at_foo.org'
+    subject.rep_email = invalid_email
+    expect(subject).not_to be_valid
+    
+    invalid_email = 'example.user@foo.foo@bar_baz.com'
+    subject.rep_email = invalid_email
+    expect(subject).not_to be_valid
+    
+    invalid_email = 'foo@bar+baz.com'
+    subject.rep_email = invalid_email
+    expect(subject).not_to be_valid
+    
+    invalid_email = 'foo@bar..com'
+    subject.rep_email = invalid_email
+    expect(subject).not_to be_valid
+    
+    # this one errors
+    # invalid_email = ' foo@bar.com'
+    # subject.rep_email = invalid_email
+    # expect(subject).to be_valids
+
+    invalid_email = 'foo@bar.com'
+    subject.rep_email = invalid_email
+    expect(subject).to be_valid
+  end
 end
 
 RSpec.describe University, type: :model do
@@ -63,19 +116,19 @@ RSpec.describe University, type: :model do
   end
 end
 
-RSpec.describe AnswerChoice, type: :model do
-  subject do
-    described_class.new(questionID: 1, answer_choice: 'Yes')
-  end
-  it 'is not valid without a num_nominees' do
-    subject.num_nominees = nil
-    expect(subject).not_to be_valid
-  end
-end
+# RSpec.describe AnswerChoice, type: :model do
+#   subject do
+#     described_class.new(questionID: 1, answer_choice: 'Yes')
+#   end
+#   it 'is not valid without a num_nominees' do
+#     subject.num_nominees = nil
+#     expect(subject).not_to be_valid
+#   end
+# end
 
 RSpec.describe Student, type: :model do
   subject do
-    @uni = University.new(university_name: 'AM')
+    @uni = University.new(university_name: 'AM', num_nominees: 0, max_limit: 3)
     @uni.save
     @rep = Representative.new(first_name: 'John', last_name: 'Smith', title: 'CEO', university_id: @uni.id, rep_email: 'JohnSmith@gmail.com')
     @rep.save
@@ -103,7 +156,6 @@ RSpec.describe Student, type: :model do
   
   it 'is not valid without a student_email' do
     subject.student_email= nil
-    #validate email
     expect(subject).not_to be_valid
   end
   
@@ -120,5 +172,47 @@ RSpec.describe Student, type: :model do
   it 'is not valid without a major' do
     subject.major = nil
     expect(subject).not_to be_valid
+  end
+  
+  it 'does not allow emails that do not fit the specified format' do
+    invalid_email = 'user'
+    subject.student_email = invalid_email
+    expect(subject).to be_invalid
+    
+    invalid_email = 'user@' 
+    subject.student_email = invalid_email
+    expect(subject).not_to be_valid
+
+    unexpected_email = 'user@foo' 
+    subject.student_email = unexpected_email
+    expect(subject).to be_valid
+    
+    invalid_email = 'user@foo,com'
+    subject.student_email = invalid_email
+    expect(subject).not_to be_valid
+    
+    invalid_email = 'user_at_foo.org'
+    subject.student_email = invalid_email
+    expect(subject).not_to be_valid
+    
+    invalid_email = 'user_at_foo.org'
+    subject.student_email = invalid_email
+    expect(subject).not_to be_valid
+    
+    invalid_email = 'example.user@foo.foo@bar_baz.com'
+    subject.student_email = invalid_email
+    expect(subject).not_to be_valid
+    
+    invalid_email = 'foo@bar+baz.com'
+    subject.student_email = invalid_email
+    expect(subject).not_to be_valid
+    
+    invalid_email = 'foo@bar..com'
+    subject.student_email = invalid_email
+    expect(subject).not_to be_valid
+
+    invalid_email = 'foo@bar.com'
+    subject.student_email = invalid_email
+    expect(subject).to be_valid
   end
 end
