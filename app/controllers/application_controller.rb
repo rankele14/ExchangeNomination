dbf65acb5a84dfa93c45b8f8741ecf4fc48403cf
Rosendo <rosendo@tamu.edu>
2022-04-11 21:54:23 -0500

    Update application_controller.rb

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,3 @@
 class ApplicationController < ActionController::Base
-  before_action :authenticate_admin!, except: [:user_new, :user_show, :user_create, :finish, :user_edit, :user_update, :edit_student_response, :user_help, :deadline, :user_delete]
+  before_action :authenticate_admin!, except: [:user_new, :user_show, :user_create, :finish, :user_edit, :user_update, :edit_student_response, :user_help, :deadline, :user_delete, :user_delete_student_url]
 end
